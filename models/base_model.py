232a49e33c6058450621f05c942a476799fd663a
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-08-06 20:55:06 +0300

    fix bug and create files for task

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """define all common attribute/method for other classes"""
-import models
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
+
+import models
 
 
 class BaseModel:
@@ -15,17 +16,18 @@ class BaseModel:
             **kwargs (dict): Key/value pairs of attributes.
         """
         time_form = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for key, val in kwargs.items():
+                if key == "__class__":
+                    continue
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(val, time_form)
                 else:
                     self.__dict__[key] = val
         else:
-            pass
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def save(self):
         """updates the updated_at public instance attribute"""
@@ -37,9 +39,9 @@ class BaseModel:
         key __class__ added to the dictionary
         """
         dicti = self.__dict__.copy()
-        dicti['created_at'] = self.created_at.isoformat()
-        dicti['updated_at'] = self.updated_at.isoformat()
-        dicti['__class__'] = self.__class__.__name__
+        dicti["created_at"] = self.created_at.isoformat()
+        dicti["updated_at"] = self.updated_at.isoformat()
+        dicti["__class__"] = self.__class__.__name__
 
         return dicti
 
