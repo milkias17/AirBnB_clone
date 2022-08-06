dfa1b8ab802d38a50b3ff87c98a8187f5f0caf86
Leulseged <leulbekele191@gmail.com>
2022-08-06 13:42:32 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """define all common attribute/method for other classes"""
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -7,11 +8,20 @@ from datetime import datetime
 class BaseModel:
     """parent class of AirBnB project"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize id, created_at, updated_at"""
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+
+        time_form = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(val, time_form)
+                elif key == '__class__':
+                    continue
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def save(self):
         """updates the updated_at public instance attribute"""
